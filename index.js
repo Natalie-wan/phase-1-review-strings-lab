42c521669935800515b65ebb1125813ebe860762
Natalie <wanjikunatalie631@gmail.com>
2024-11-27 19:04:44 +0300

    reviewing strings

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = 'Natalie Wanjiku';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//const shortGreeting = 'Welcome, ';
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
